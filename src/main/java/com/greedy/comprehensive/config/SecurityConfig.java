bdc3f2f0f3832b01716a840fbfae14a7fc87e0ff
byeollim woo <byeollim.woo@gmail.com>
2023-03-09 10:37:24 +0900

    commit

--- src/main/java/com/greedy/comprehensive/config/SecurityConfig.java
+++ src/main/java/com/greedy/comprehensive/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import com.greedy.comprehensive.member.service.AuthenticationService;
 
 @EnableWebSecurity
 public class SecurityConfig {
