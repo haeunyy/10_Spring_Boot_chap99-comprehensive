74d12aefb051c359e0cd76c028ea5d04db232cd8
byeollim woo <byeollim.woo@gmail.com>
2023-03-09 10:12:48 +0900

    commit

---
+++ src/main/java/com/greedy/comprehensive/Chap99ComprehensiveApplication.java
@@ -0,0 +1,13 @@
+package com.greedy.comprehensive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap99ComprehensiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap99ComprehensiveApplication.class, args);
+	}
+
+}
