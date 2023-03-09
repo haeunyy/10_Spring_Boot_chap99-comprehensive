386cc3d55d183dd6da575467e9e1b2022b3d306d
byeollim woo <byeollim.woo@gmail.com>
2023-03-09 10:30:18 +0900

    commit

---
+++ src/main/java/com/greedy/comprehensive/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.greedy.comprehensive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import com.greedy.comprehensive.member.service.AuthenticationService;
+
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+        return http
+        		.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/board/**", "/thumbnail/**", "/member/update", "/member/delete").hasRole("MEMBER")
+                // 관리자만 사용 가능한 기능은 현재는 없음
+                .anyRequest().permitAll()
+                .and()
+                    .formLogin()
+                    .loginPage("/member/login")             
+                    .defaultSuccessUrl("/")  
+                    .failureForwardUrl("/member/loginfail")
+                    .usernameParameter("memberId")			// 아이디 파라미터명 설정
+                    .passwordParameter("memberPwd")			// 패스워드 파라미터명 설정
+                .and()
+                    .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                    .deleteCookies("JSESSIONID")
+                    .invalidateHttpSession(true)
+                    .logoutSuccessUrl("/")
+                // 따라서 인가 오류 처리는 생략하였음
+    			.and()
+    				.build();
+    }
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
